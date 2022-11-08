66098ce2777f9119a3fef043579d036215edb39c
koushik <koushiksk.ks@gmail.com>
2022-11-08 19:10:14 +0600

    initial commit

---
+++ components/layout/Header/Header.js
@@ -0,0 +1,13 @@
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <>
+      <header className={styles.navbar}>
+        This is the header
+      </header>
+    </>
+  );
+};
+
+export default Header;
