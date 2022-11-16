0e8fbd59d62743e40f6a7013797956e33d38c9e6
koushik <koushiksk.ks@gmail.com>
2022-11-16 23:31:15 +0600

    banner add

--- pages/components/Slider/Slider.js
+++ pages/components/Slider/Slider.js
@@ -1,6 +1,6 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import {Swiper, SwiperSlide} from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
@@ -10,60 +10,52 @@ import "./Slider.module.scss";
 
 // import required modules
 import {Autoplay, Pagination} from "swiper";
-// import img from '../../public/assets/imgs/img.png'
+import img1 from '../../../public/assets/imgs/prfume_3_generated.jpg'
+import img2 from '../../../public/assets/imgs/perfume.jpg'
+import img3 from '../../../public/assets/imgs/sanitizer.jpg'
 import Image from 'next/image';
+
 export default function Slider() {
-    return (
-        <>
-            <Swiper
-                autoplay={{
-                    delay: 5000,
-                    disableOnInteraction: false,
-                }}
-                pagination={{
-                    dynamicBullets: true,
-                }}
-                modules={[Autoplay,Pagination]}
-                className="mySwiper"
-            >
-                <SwiperSlide className={''}>
-                    <Image src={'/assets/imgs/home4.jpg'} alt="logo"
-                           height={2}
-                           width={1800}
-                           className={'h-screen w-full object-cover'}
-                    />
+  return (
+    <>
+      <Swiper
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Autoplay, Pagination]}
+        className="mySwiper HomeSlider"
+      >
+        <SwiperSlide className={''}>
+          <Image src={img2} alt="logo"
+                 height={2}
+                 width={1800}
+                 className={'h-screen w-full object-cover'}
+          />
 
-                </SwiperSlide>
-                <SwiperSlide className={''}>
-                    <Image src={'/assets/imgs/home3.jpg'} alt="logo"
-                           height={2}
-                           width={1800}
-                           className={'h-screen w-full object-cover'}
-                    />
-                </SwiperSlide>
+        </SwiperSlide>
+        <SwiperSlide className={''}>
+          <Image src={img1} alt="logo"
+                 className={'h-screen w-full object-cover'}
+          />
+        </SwiperSlide>
 
-                <SwiperSlide className={''}>
-                    <Image src={'/assets/imgs/home2.jpg'} alt="logo"
-                           height={2}
-                           width={1800}
-                           className={'h-screen w-full object-cover'}
-                    />
-                </SwiperSlide>
-                <SwiperSlide className={''}>
-                    <Image src={'/assets/imgs/home1.jpg'} alt="logo"
-                           height={2}
-                           width={1800}
-                           className={'h-screen w-full object-cover'}
-                    />
-                </SwiperSlide>
-                <SwiperSlide className={''}>
-                    <Image src={'/assets/imgs/img2.jpg'} alt="logo"
-                           height={2}
-                           width={1800}
-                           className={'h-screen w-full object-cover'}
-                    />
-                </SwiperSlide>
-            </Swiper>
-        </>
-    );
+        <SwiperSlide className={''}>
+          <Image src={img3} alt="logo"
+                 className={'h-screen w-full object-cover'}
+          />
+        </SwiperSlide>
+        <SwiperSlide className={''}>
+          <Image src={'/assets/imgs/img2.jpg'} alt="logo"
+                 height={2}
+                 width={1800}
+                 className={'h-screen w-full object-cover'}
+          />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
