4ed898095b6297d4e0277fc09db07edcad9d2a8c
Bishajit <bishajitchakraborty01@gmail.com>
2022-11-19 23:01:42 +0600

    client full design complete with responsive

--- pages/client/index.js
+++ pages/client/index.js
@@ -9,13 +9,16 @@ const Index = () => {
                <ClientSlider/>
             </div>
             <div className={'bg-blue-white py-20'}>
-                <h2 className={'text-center text-2xl italic text-green-600'}>Our Few Client Lists</h2>
-                <div className={'w-8/12 mx-auto mt-10 flex flex-wrap py-2'}>
+                <h2 className={'text-center text-2xl italic text-gray-400'}>Our Few Client Lists</h2>
+                <div className={'w-8/12 mx-auto mt-10 flex flex-wrap '}>
                     {[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1].map((v,i)=>(
-                        <div className={'mobile:w-1/4 w-full shadow-2xl mt-2'} key={i}>
-                            <div className={'mr-2 bg-green-500'}>
-                                <img src={'/assets/imgs/IFAD-Multi-Products-Ltd.png'} className={'h-36 p-2 scale-50 hover:scale-75 ease-in duration-500 w-full'}/>
+                        <div className={'mobile:w-1/4 w-full  mt-4'} key={i}>
+                            <div className={'mx-2'}>
+                                <div className={'shadow bg-white'}>
+                                    <img src={'/assets/imgs/IFAD-Multi-Products-Ltd.png'} className={'h-36 p-2 scale-50 hover:scale-75 ease-in duration-500 w-full'}/>
+                                </div>
                             </div>
+
 
                         </div>
                     ))}
