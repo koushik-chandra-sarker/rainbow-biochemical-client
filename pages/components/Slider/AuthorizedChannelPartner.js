19486247532fe19dd41474833a5a75ed3ef56dd7
Bishajit <bishajitchakraborty01@gmail.com>
2022-11-20 15:17:46 +0600

    Home page complete

--- pages/components/Slider/AuthorizedChannelPartner.js
+++ pages/components/Slider/AuthorizedChannelPartner.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Swiper, SwiperSlide} from "swiper/react";
-import {Navigation, Pagination} from "swiper";
+import {Autoplay, Navigation, Pagination} from "swiper";
 import Image from "next/image";
 import img from '../../../public/assets/imgs/p2.jpg'
 
@@ -9,38 +9,42 @@ const AuthorizedChannelPartner = () => {
         <div>
             <Swiper
                 style={{
-                    // "--swiper-navigation-color": "rgb(92,232,95)",
-                    // "--swiper-pagination-color": "rgb(92,232,95)",
+                    "--swiper-navigation-color": "rgb(92,232,95)",
+                    "--swiper-pagination-color": "rgb(92,232,95)",
+                }}
+                autoplay={{
+                    delay: 5000,
+                    disableOnInteraction: false,
                 }}
                 pagination={{
                     clickable: true,
                 }}
                 navigation={true}
-                modules={[Pagination, Navigation]}
+                modules={[Autoplay,Pagination, Navigation]}
                 className="related-products-slider"
                 breakpoints={{
                     0: {
                         slidesPerView: 2,
-                        spaceBetween: 2,
+                        spaceBetween: 4,
                     },
                     640: {
                         slidesPerView: 3,
-                        spaceBetween: 2,
+                        spaceBetween: 4,
                     },
                     768: {
                         slidesPerView: 4,
-                        spaceBetween: 2,
+                        spaceBetween: 4,
                     },
                     1024: {
                         slidesPerView: 5,
-                        spaceBetween: 2,
+                        spaceBetween: 4,
                     }
                 }}
             >
                 {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((v, i) => (
-                    <SwiperSlide className={"h-full "} key={i} >
-                        <div className={'flex flex-col bg-white items-center gap-5 p-5 cursor-pointer'}>
-                            <Image src={img} className={"object-contain tablet:h-36 h-24"}/>
+                    <SwiperSlide className={"h-full"} key={i} >
+                        <div className={'flex flex-col bg-white items-center gap-4 p-5 cursor-pointer'}>
+                            <Image src={img} className={"object-contain tablet:h-24 h-24 w-32 "}/>
                         </div>
                     </SwiperSlide>
 
--- pages/components/Slider/Slider.js
+++ pages/components/Slider/Slider.js
@@ -1,9 +1,6 @@
 import React from "react";
 // Import Swiper React components
 import {Swiper, SwiperSlide} from "swiper/react";
-
-// import "./Slider.scss";
-// import required modules
 import {Autoplay, Pagination} from "swiper";
 import img1 from '../../../public/assets/imgs/prfume_3_generated.jpg'
 import img2 from '../../../public/assets/imgs/perfume.jpg'
@@ -25,33 +22,50 @@ export default function Slider() {
         modules={[Autoplay, Pagination]}
         className="mySwiper home-slider"
       >
-        <SwiperSlide className={''}>
+        <SwiperSlide>
           <div className={"relative"}>
             <Image src={img2} alt="logo"
                    height={2}
                    width={1800}
                    className={'w-full'}
             />
-            <button className={"absolute top-1/4 left-1/4 z-10 "}>Details</button>
+            <div className={'absolute top-2/4 left-1/4 z-10 animate__bounce'}>
+              <button className={" bg-blue-600 text-white rounded-3xl px-8 py-2  "} >Details</button>
+            </div>
           </div>
 
 
         </SwiperSlide>
-        <SwiperSlide className={''}>
-          <Image src={img1} alt="logo"
-                 className={' w-full'}
-          />
+        <SwiperSlide>
+          <div className={'relative'}>
+            <Image src={img1} alt="logo"
+                   className={' w-full'}
+            />
+          </div>
+          <div className={'absolute top-2/4 left-1/4 z-10 animate__bounce'}>
+            <button className={" bg-blue-600 text-white rounded-3xl px-8 py-2  "} >Details</button>
+          </div>
         </SwiperSlide>
 
-        <SwiperSlide className={''}>
-          <Image src={img3} alt="logo"
-                 className={'w-full'}
-          />
+        <SwiperSlide>
+          <div className={'relative'}>
+            <Image src={img3} alt="logo"
+                   className={'w-full'}
+            />
+          </div>
+          <div className={'absolute top-2/4 left-1/4 z-10 animate__bounce'}>
+            <button className={" bg-blue-600 text-white rounded-3xl px-8 py-2  "} >Details</button>
+          </div>
         </SwiperSlide>
-        <SwiperSlide className={''}>
-          <Image src={img4} alt="logo"
-                 className={' w-full'}
-          />
+        <SwiperSlide>
+          <div className={'relative'}>
+            <Image src={img4} alt="logo"
+                   className={' w-full'}
+            />
+          </div>
+          <div className={'absolute top-2/4 left-1/4 z-10 animate__bounce'}>
+            <button className={" bg-blue-600 text-white rounded-3xl px-8 py-2  "} >Details</button>
+          </div>
         </SwiperSlide>
       </Swiper>
     </>
