d8e13708e6bd223033e88045509a121a7773d67a
koushik <koushiksk.ks@gmail.com>
2022-11-24 21:54:25 +0600

    image remote hostname

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['file.rainbowgroup-bd.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'file.rainbowgroup-bd.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
